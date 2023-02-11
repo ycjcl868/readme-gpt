060a98c97b82014a402ca4251cc6e3e43c71bb91
ycjcl868 <chaolinjin@gmail.com>
2023-02-12 07:08:29 +0800

    fix: top margin

--- pages/index.tsx
+++ pages/index.tsx
@@ -115,7 +115,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-      <main className='flex flex-1 w-full flex-col items-center justify-center mt-0'>
+      <main className='flex flex-1 w-full flex-col items-center justify-center mt-4'>
         <div className='flex items-center justify-center mb-5'>
           <a
             className='flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mr-3'
