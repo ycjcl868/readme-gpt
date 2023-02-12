6f78cb0a53919e2368a21059f91b7da53df84f21
ycjcl868 <chaolinjin@gmail.com>
2023-02-12 21:32:22 +0800

    chore: optimize css

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  optimizeFonts: true,
+  experimental: {
+    optimizeCss: true
+  },
   i18n: {
     locales: ['en', 'zh'],
     defaultLocale: 'en'
